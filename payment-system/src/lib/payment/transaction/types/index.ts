dd0532d8ba626d03ef3edfda93a79811eb62d64d
crowka1 <key_to@yahoo.com>
2025-03-20 11:21:11 +0100

    Update index.ts

--- payment-system/src/lib/payment/transaction/types/index.ts
+++ payment-system/src/lib/payment/transaction/types/index.ts
@@ -1,3 +1,25 @@
-import { Transaction, TransactionType, TransactionStatus, TransactionError } from '../../types/transaction.types';
-export { Transaction, TransactionType, TransactionStatus, TransactionError };
+// src/lib/payment/transaction/types/index.ts
 
+// Import from the central types file instead of a relative path
+import { 
+  Transaction, 
+  TransactionType, 
+  TransactionStatus, 
+  TransactionError,
+  TransactionErrorCode,
+  TransactionFailedError,
+  RetryPolicy,
+  RecoveryStrategy
+} from '../../types/transaction.types';
+
+// Re-export all transaction types
+export { 
+  Transaction, 
+  TransactionType, 
+  TransactionStatus, 
+  TransactionError,
+  TransactionErrorCode,
+  TransactionFailedError,
+  RetryPolicy,
+  RecoveryStrategy
+};
